aaf90de56ad5a71da3e83f7bc9cf527706bda1d7
Diegfo Scolnik <diego.scolnik@gmail.com>
2021-08-15 19:49:11 -0300

    Clase V

---
+++ operadores_lógicos.py
@@ -0,0 +1,15 @@
+#and
+#or
+
+a = 2
+b = 3
+
+if(a == 2 and b == 1):
+    print('Valor correcto')
+else:
+    print('Valor incorrecto')
+
+if(a == 7 or b == 3):
+    print('Valor correcto')
+else:
+    print('Valor incorrecto')
